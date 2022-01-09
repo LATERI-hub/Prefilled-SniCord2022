c114b44d2ef2368d06d5fb2c0d315af1e059a879
Sebastian09-09 <73454646+Sebastian09-09@users.noreply.github.com>
2022-01-08 23:26:34 -0800

    new feature

--- spammer.py
+++ spammer.py
@@ -3,21 +3,28 @@ import requests
 import random
 import string
 import time
+from colorama import Fore, Style , init;init()
 
 class Spammer:
     threads = {}
-    def sendMessage(channelID , token , n):
+    def sendMessage(channelID , token , n , channelName):
         messagesSent = 0
         while messagesSent < n:
             time.sleep(5)
+            if not str(channelID) in Spammer.threads:
+                break
             msg = str(''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(1,10))))
             payload = {"content" : msg}
             r = requests.post(f'https://discord.com/api/v9/channels/{channelID}/messages' , data=payload , headers={"authorization" : token} )
             if r.status_code == 200:
                 messagesSent += 1
-        del Spammer.threads[str(channelID)]
 
-#you can change the time in line 12 
+        if str(channelID) in Spammer.threads:
+            del Spammer.threads[str(channelID)]
+        
+        print(Fore.YELLOW + Style.BRIGHT + f'Spam completed in channel {channelName} : {channelID}')
+
+#you can change the time in line 13 
 #but if you reduce the time more and more
 #your account might start getting ratelimited and
 #discord could temp. ban your account from accesing there API .
