8af45e63bc8210c46ef1a0e4d36d28fa377ae792
Sebastian09-09 <73454646+Sebastian09-09@users.noreply.github.com>
2022-01-09 00:12:49 -0800

    fill in your details

---
+++ configmaker.py
@@ -0,0 +1,51 @@
+import os
+import json
+
+files = os.listdir()
+
+def getConfig():
+    with open('config.json' , 'r' , encoding='utf-8') as c:
+        return json.load(c)
+
+def setConfig(data):
+    with open('config.json' , 'w' , encoding='utf-8') as c:
+        json.dump(data , c)
+
+if 'config.json' not in files:
+    with open('config.json' , 'w' , encoding='utf-8') as f:
+        f.write('''{
+    "prefix": "->", 
+    "token": "", 
+    "anigameSniper": "", 
+    "izziSniper": "", 
+    "latency": "0", 
+    "respond": "", 
+    "baseChannelID": "", 
+    "baseChannelAnigamePrefix": ".", 
+    "baseChannelIzziPrefix": "iz ", 
+    "anigameLottery": "", 
+    "anigameHourly": "", 
+    "izziLottery": "",
+    "izziHourly": ""
+}''')
+
+configdat = getConfig()
+for i in configdat:
+    if i == 'anigameSniper' \
+    or i == 'izziSniper' \
+    or i == 'respond' \
+    or i == 'anigameLottery'\
+    or i == 'anigameHourly'\
+    or i == 'izziLottery'\
+    or i == 'izziHourly':
+        value = input(f'{i} (on/off) : ')
+
+    elif i== 'latency':
+        value = input(f'{i} (number) : ')
+    
+    else:
+        value=input(f'{i} : ')
+    
+    configdat[i] = value
+setConfig(configdat)
+input('completed! , press space to continue ')
