4b02c2b3882de60ab154a9b60933c331dd2eb3fd
Sebastian09-09 <73454646+Sebastian09-09@users.noreply.github.com>
2022-01-13 09:37:23 -0800

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -8,7 +8,21 @@ from colorama import Fore, Style , init;init()
 class Spammer:
     threads = {}
     def sendMessage(channelID , token , n , channelName):
-        if n.isnumeric():
+        if n == '!':
+            while True:
+                time.sleep(5)
+                if not str(channelID) in Spammer.threads:
+                    break
+                msg = str(''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(1,10))))
+                payload = {"content" : msg}
+                r = requests.post(f'https://discord.com/api/v9/channels/{channelID}/messages' , data=payload , headers={"authorization" : token} )
+
+            if str(channelID) in Spammer.threads:
+                del Spammer.threads[str(channelID)]
+            
+            print(Fore.YELLOW + Style.BRIGHT + f'Spam completed in channel {channelName} : {channelID}')
+            
+        else:
             messagesSent = 0
             while messagesSent < n:
                 time.sleep(5)
@@ -23,23 +37,10 @@ class Spammer:
             if str(channelID) in Spammer.threads:
                 del Spammer.threads[str(channelID)]
             
-            print(Fore.YELLOW + Style.BRIGHT + f'Spam completed in channel {channelName} : {channelID}')
-        elif n == '!':
-            while True:
-                time.sleep(5)
-                if not str(channelID) in Spammer.threads:
-                    break
-                msg = str(''.join(random.choices(string.ascii_uppercase + string.digits, k=random.randint(1,10))))
-                payload = {"content" : msg}
-                r = requests.post(f'https://discord.com/api/v9/channels/{channelID}/messages' , data=payload , headers={"authorization" : token} )
-
-            if str(channelID) in Spammer.threads:
-                del Spammer.threads[str(channelID)]
-            
-            print(Fore.YELLOW + Style.BRIGHT + f'Spam completed in channel {channelName} : {channelID}')
+            print(Fore.YELLOW + Style.BRIGHT + f'Spam completed in channel {channelName} : {channelID}') 
 
 
-#you can change the time in line 14 and 29
+#you can change the time in line 13 and 28
 #but if you reduce the time more and more
 #your account might start getting ratelimited and
 #discord could temp. ban your account from accesing there API .
